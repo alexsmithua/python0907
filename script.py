6e76d07f389b87a98f101aee52528150d220a113
alexsmithua <siroco.meta@gmail.com>
2016-07-10 18:57:55 +0300

    My first commit

---
+++ script.py
@@ -0,0 +1,29 @@
+def my_sum (x):
+    if x%2 ==1:
+        print("nechet")
+    else:
+        print("chet")
+
+my_sum(50)
+
+#num = int(input("Enter num: "))
+#print("you squared : " )
+
+#def you_
+num = int(input("Vash god: "))
+old = 2016-num
+print(2016-num)
+if old < 18:
+    print("PAHO PIT")
+elif old <= 60:
+    print("B MERY")
+elif old <=100:
+    print("LEKARSTVO")
+else:
+    print("POZDHO PIT")
+
+x=0
+while x < 10:
+    print(x*x)
+    x +=1
+
